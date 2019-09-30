33e63bc8247999225482b64c7d54fe87be9c7aa7
carlosalanis <juancarlos.alanis@hotmail.com>
2019-09-30 10:30:42 -0500

    OCR Estudio 16

--- app/src/main/java/org/lavid/hogares/oracionesMenu.java
+++ app/src/main/java/org/lavid/hogares/oracionesMenu.java
@@ -15,7 +15,7 @@ public class oracionesMenu extends AppCompatActivity {
 
     CardView cardOCR1; CardView cardOCR2; CardView cardOCR3; CardView cardOCR4; CardView cardOCR5;
     CardView cardOCR9; CardView cardOCR10; CardView cardOCR11; CardView cardOCR12; CardView cardOCR13;
-    CardView cardOCR14; CardView cardOCR15;
+    CardView cardOCR14; CardView cardOCR15; CardView cardOCR16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,6 +147,16 @@ public class oracionesMenu extends AppCompatActivity {
             public void onClick(View v) {
                 Intent mainIntent = new Intent(getApplicationContext(), chapters.class);
                 mainIntent.putExtra("cap", "oracion_pablo_publio.html");
+                startActivity(mainIntent);
+            }
+        });
+
+
+        cardOCR16 = findViewById(R.id. card_view16);
+        cardOCR16.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(getApplicationContext(), chapters.class);
+                mainIntent.putExtra("cap", "oracion_cornelio.html");
                 startActivity(mainIntent);
             }
         });
