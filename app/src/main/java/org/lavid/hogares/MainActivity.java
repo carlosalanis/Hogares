0479682aa67f5309a74417987032af9cbac85add
carlosalanis <juancarlos.alanis@hotmail.com>
2019-10-14 16:20:22 -0500

    OCR18a

--- app/src/main/java/org/lavid/hogares/MainActivity.java
+++ app/src/main/java/org/lavid/hogares/MainActivity.java
@@ -26,24 +26,15 @@ import android.widget.Toast;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     ImageView imgSermon;
     ImageView imgLectura;
     ImageView imgBiblia;
 
-    ViewPager viewPager;
-    Integer[] imageId = {
-            R.drawable.imghogares, R.drawable.img15, R.drawable.img07, R.drawable.img22, R.drawable.img04,
-            R.drawable.img26, R.drawable.img25, R.drawable.img09, R.drawable.img28, R.drawable.img13,
-            R.drawable.img14, R.drawable.img02, R.drawable.img16, R.drawable.img17, R.drawable.img29,
-            R.drawable.img19, R.drawable.img21, R.drawable.img05, R.drawable.img23, R.drawable.img24,
-            R.drawable.img08, R.drawable.img18, R.drawable.img27, R.drawable.img01, R.drawable.img06 };
-    int currentPage = 0;
-    int NUM_PAGES = 27;
+
     private static final int REQUEST = 112;
-    Timer timer;
+
 
      @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,34 +58,7 @@ public class MainActivity extends AppCompatActivity
          }
 
 
-        /* IMAGE PAGER */
-        viewPager = findViewById(R.id.autoviewpager);
-        PagerAdapter adapter = new CustomAdapter(MainActivity.this,imageId);
-        viewPager.setAdapter(adapter);
-        viewPager.bringToFront();
-        final Handler handler = new Handler();
-        final Runnable Update = new Runnable() {
-            public void run() {
-                if (currentPage == NUM_PAGES-1) {
-                    currentPage = 0;
-                }
-                viewPager.setCurrentItem(currentPage++, true);
-            }
-        };
-        timer = new Timer(); // This will create a new Thread
-        timer .schedule(new TimerTask() { // task to be scheduled
-            @Override
-            public void run() {
-                handler.post(Update);
-            }
-        }, 1000, 10000);
 
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
 
          imgLectura = (ImageView) findViewById(R.id.imgLectura);
          imgLectura.setOnClickListener(new View.OnClickListener() {
@@ -122,9 +86,6 @@ public class MainActivity extends AppCompatActivity
              }
          });
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
     }
 
     @Override
@@ -137,12 +98,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -160,23 +115,7 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
 
     @Override
