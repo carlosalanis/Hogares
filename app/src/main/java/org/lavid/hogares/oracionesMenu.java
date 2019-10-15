59604bcf090e63705e1b9d488f73272186ae9df4
carlosalanis <juancarlos.alanis@hotmail.com>
2019-10-15 10:10:14 -0500

    OCR19

--- app/src/main/java/org/lavid/hogares/oracionesMenu.java
+++ app/src/main/java/org/lavid/hogares/oracionesMenu.java
@@ -16,6 +16,7 @@ public class oracionesMenu extends AppCompatActivity {
     CardView cardOCR1; CardView cardOCR2; CardView cardOCR3; CardView cardOCR4; CardView cardOCR5;
     CardView cardOCR9; CardView cardOCR10; CardView cardOCR11; CardView cardOCR12; CardView cardOCR13;
     CardView cardOCR14; CardView cardOCR15; CardView cardOCR16; CardView cardOCR17; CardView cardOCR18;
+    CardView cardOCR19;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -178,6 +179,19 @@ public class oracionesMenu extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+
+        cardOCR19 = findViewById(R.id. card_view19);
+        cardOCR19.setVisibility(View.GONE);
+        if(week>=43) {
+            cardOCR19.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent mainIntent = new Intent(getApplicationContext(), chapters.class);
+                    mainIntent.putExtra("cap", "oracion_publicano.html");
+                    startActivity(mainIntent);
+                }
+            });
+            cardOCR19.setVisibility(View.VISIBLE);
+        }
 
     }
 }
